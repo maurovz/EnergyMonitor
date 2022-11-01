38d760945aa95b2392c208d50609871a612e3175
Mauricio Vazquez <mvazquez@woonkly.com>
2022-11-01 16:44:00 +0100

    Init

---
+++ EnergyMonitor/EnergyMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyMonitorApp.swift
+//  EnergyMonitor
+//
+//  Created by Mauricio Vazquez on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct EnergyMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
