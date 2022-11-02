01e27ca03d5aa702eaeba1fe31332a9d6304af3b
Mauricio Vazquez <mvazquez@woonkly.com>
2022-11-02 12:06:29 +0100

    Create LiveData and History models and loaders

---
+++ EnergyDataFeature/Historic Data Feature/History.swift
@@ -0,0 +1,20 @@
+public struct History {
+  public let buildingPower: Decimal
+  public let gridPower: Decimal
+  public let pvPower: Decimal
+  public let quasarsPower: Decimal
+  public let timeStamp: Date
+  
+  public init(
+    buildingPower: Decimal,
+    gridPower: Decimal,
+    pvPower: Decimal,
+    quasarsPower: Decimal,
+    timeStamp: Date) {
+      self.buildingPower = buildingPower
+      self.gridPower = gridPower
+      self.pvPower = pvPower
+      self.quasarsPower = quasarsPower
+      self.timeStamp = timeStamp
+  }
+}
