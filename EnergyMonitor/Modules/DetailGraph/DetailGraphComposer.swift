a3aeefeef41e4e7a4697966682eb9171d8325446
Mauricio Vazquez <maurovz@gmail.com>
2022-11-04 16:14:41 +0100

    Create DetailGraph view plotting real historic data

---
+++ EnergyMonitor/Modules/DetailGraph/DetailGraphComposer.swift
@@ -0,0 +1,8 @@
+//
+//  DetailGraphComposer.swift
+//  EnergyMonitor
+//
+//  Created by Mauricio Vazquez on 4/11/22.
+//
+
+import Foundation
---
+++ EnergyMonitor/Modules/DetailGraph/DetailGraphView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailGraphView.swift
+//  EnergyMonitor
+//
+//  Created by Mauricio Vazquez on 4/11/22.
+//
+
+import SwiftUI
+
+struct DetailGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailGraphView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailGraphView()
+    }
+}
