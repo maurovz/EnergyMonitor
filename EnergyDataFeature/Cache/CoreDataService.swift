72a226c4de8445cd5b781fb9cc3c665a3d907dac
Mauricio Vazquez <maurovz@gmail.com>
2022-11-06 12:11:50 +0100

    Create HistoricEntity and LiveDataEntity CoreData services

--- EnergyDataFeature/Cache/CoreDataService.swift
+++ EnergyDataFeature/Cache/CoreDataService.swift
@@ -3,6 +3,21 @@ import CoreData
 public struct CoreDataServices {
   public init() { }
 
+  public func deleteEntityFromCoreData(entity: String) -> Bool {
+    let context = CoreDataStack.persistentContainer.viewContext
+    do {
+      let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+      let request = NSBatchDeleteRequest(fetchRequest: fetch)
+      try context.execute(request)
+      return true
+    } catch {
+      return false
+    }
+  }
+}
+
+// MARK: - Historic Data Service
+extension CoreDataServices {
   public func saveHistoricDataToCache(data: [History]) {
     let context = CoreDataStack.persistentContainer.viewContext
 
@@ -37,24 +52,59 @@ public struct CoreDataServices {
     _ = deleteEntityFromCoreData(entity: "HistoryEntity")
   }
 
-  public func deleteEntityFromCoreData(entity: String) -> Bool {
-    let context = CoreDataStack.persistentContainer.viewContext
-    do {
-      let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
-      let request = NSBatchDeleteRequest(fetchRequest: fetch)
-      try context.execute(request)
-      return true
-    } catch {
-      return false
-    }
-  }
-
   private func fetchHistoricDataEntity() -> [HistoryEntity] {
     let context = CoreDataStack.persistentContainer.viewContext
     do {
       return try context.fetch(HistoryEntity.fetchRequest())
     } catch {
       return []
+    }
+  }
+}
+
+// MARK: - Live Data Service
+extension CoreDataServices {
+  public func saveLiveDataToCache(data: LiveData) {
+    let context = CoreDataStack.persistentContainer.viewContext
+
+    let entity = LiveDataEntity(context: context)
+    entity.solarPower = data.solarPower
+    entity.quasarPower = data.quasarPower
+    entity.gridPower = data.gridPower
+    entity.buildingDemand = data.buildingDemand
+    entity.systemSoc = data.systemSoc
+    entity.totalEnergy = data.totalEnergy
+    entity.currentEnergy = data.currentEnergy
+    CoreDataStack.saveContext()
+  }
+
+  public func loadLiveDataFromCoreData() -> LiveData? {
+    guard let entity = fetchLiveDataEntity() else { return nil }
+
+    let liveData = LiveData(
+      solarPower: entity.solarPower,
+      quasarPower: entity.quasarPower,
+      gridPower: entity.gridPower,
+      buildingDemand: entity.buildingDemand,
+      systemSoc: entity.systemSoc,
+      totalEnergy: entity.totalEnergy,
+      currentEnergy: entity.currentEnergy)
+
+    return liveData
+  }
+
+  public func deleteLiveDataEntityFromCoreData() {
+    _ = deleteEntityFromCoreData(entity: "LiveDataEntity")
+  }
+
+  private func fetchLiveDataEntity() -> LiveDataEntity? {
+    let context = CoreDataStack.persistentContainer.viewContext
+    do {
+      let request = LiveDataEntity.fetchRequest()
+      let liveData = try context.fetch(request)
+      return liveData.first
+    } catch {
+      return nil
     }
   }
 }
